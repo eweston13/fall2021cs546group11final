60c98f401976b5d8a0faa38d31968d00780e8192
Prathamesh1026 <shelarprathamesh9@gmail.com>
2021-11-25 17:37:57 -0500

    init

--- Project/config/mongoCollections.js
+++ Project/config/mongoCollections.js
@@ -1,7 +1,5 @@
 const dbConnection = require('./mongoConnection');
 
-/* This will allow you to have one reference to each collection per app */
-/* Feel free to copy and paste this this */
 const getCollectionFn = (collection) => {
   let _col = undefined;
 
@@ -18,5 +16,6 @@ const getCollectionFn = (collection) => {
 /* Now, you can list your collections here: */
 module.exports = {
   students: getCollectionFn('students'),
-  instructors: getCollectionFn('instructors')
+  instructors: getCollectionFn('instructors'),
+  quizzes: getCollectionFn('quizzes')
 };
