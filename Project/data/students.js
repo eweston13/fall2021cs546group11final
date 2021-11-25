2c85b5148c1d3c8c9ee7c77e869292183905c111
Uday Samvenkata <usamaven@stevens.edu>
2021-11-25 16:47:25 -0500

    made some changes to the students and instructors

--- Project/data/students.js
+++ Project/data/students.js
@@ -1,26 +1,64 @@
 const mongoCollections = require('../config/mongoCollections');
 const students = mongoCollections.students;
+const bcrypt = require('bcrypt');
+const saltRounds = 16;
 
-let exportedMethods = {
-    async addStudent(firstName, lastName, email, password, lessonsViewed, quizzesCompleted) {
-        const studentCollection = await students();
 
-        let newUser = {
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        password: password,
-        lessonsViewed: "placeholder",
-        quizzesCompleted: "placeholder"
-        }
+async function addStudent(firstName, lastName, email, username, password, lessonsViewed, quizzesCompleted) {
+    const studentCollection = await students();
 
-        const newInsertInformation = await studentCollection.insertOne(newUser);
-        if (newInsertInformation.insertedCount === 0) throw 'Insert failed!';
-
-        return newInsertInformation
+    let newUser = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    username: username,
+    password: await bcrypt.hash(password, saltRounds),
+    lessonsViewed: "placeholder",
+    quizzesCompleted: "placeholder"
     }
-};
 
-module.exports = exportedMethods;
+    //REMEMBER TO MAKE USERNAMES LOWERCASE
+    const newInsertInformation = await studentCollection.insertOne(newUser);
+    if (newInsertInformation.insertedCount === 0) throw 'Insert failed!';
+
+    return newInsertInformation
+}
+
+async function checkStudent(uname, pass){
+
+
+    const studentCollection = await students()
+
+    let obj = await studentCollection.findOne({username: uname})
+
+    var passCheck
+    try{
+        passCheck = await bcrypt.compare(pass, obj.password);
+    }catch(e){
+        console.log(e)
+    }
+
+    if(passCheck && uname == obj.username){
+        return {authenticated: true}
+    }
+
+    throw "Either the username or password is invalid"
+
+}
+
+//function test is used just to test individual functions to see if they work, seed file must be run first
+async function test(){
+    let student1verif = await checkStudent("udaysama17", "Password")
+    console.log("Student check:", student1verif)
+}
+
+//  test()
+
+
+
+module.exports = {
+    addStudent,
+    checkStudent
+}
 
 
