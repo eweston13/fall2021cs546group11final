44afd21edd66d90ff40deb97866756c8e8e467ec
Uday Samvenkata <usamaven@stevens.edu>
2021-12-06 12:27:55 -0500

    little more quiz handlebar stuff

--- Project/public/js/quizCreate.js
+++ Project/public/js/quizCreate.js
@@ -1,12 +1,17 @@
 (function($){
+    // console.log("test")
+    $.ajax('/quizEditPost/quizCreate').then(function(stuff){
+        console.log(stuff)
+        $('#mcAdd').on('click', function(event){
+            event.preventDefault()
+            $('#questionList').append('<li>Test</li>')
+            createQuestion()
 
-
-
-
-
-
-
-
-
-
-})(window.jQuery);
+        })
+    })
+    
+         
+  
+})(window.jQuery)
+  
+  
